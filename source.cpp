2aeac6105a2d63d3f78621b5c7e0e8363b210d39
tranthingocnhat <DH51705101@student.stu.edu.vn>
2020-10-24 16:43:01 +0700

    chu vi dien tich HCN & HV

--- source.cpp
+++ source.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 using namespace std;
-int tong(int a, int b);
-int hieu(int a, int b);
-int tich(int a, int b);
-float thuong(int a, int b);
-int main()
-{
+
+int pheptoan(){
 	int a, b;
 	char phepToan;
 	cout << "Nhap 2 so nguyen: ";
 	cin >> a >> b;
 	cout << "Tinh gi? (+,-,*,/) ";
 	cin >> ch;
+	
 	switch (phepToan)
 	{
 	case '+':
@@ -28,9 +25,83 @@ int main()
 			cout<<"Ko chia cho 0 dc"
 		else cout<<"a/b ="<<a/b;
 		break;
+		
+		
+	}
+}
+void chuViDienTich(){
+	int tinhCV;
+	
+
+	int tinhCVhcn(){
+		int d,r;
+		cout<<"nhap CD , CR"
+		cin>> d>>r;
+		return (d+r)*2;
+	}
+	int tinhDThcn(){
+		int d,r;
+		cout<<"nhap CD , CR"
+		cin>> d>>r;
+		return d*r;
+	}
+	int tinhCVhinhvuong(){
+		int d;
+		cout<<"nhap canh"
+		cin>> d;
+		return d*4;
+	}
+	int tinhDThinhvuong(){
+	int d;
+	cout<<"nhap canh"
+	cin>> d;
+	return d*d;
+}
+	
+	cout<<"nhap 1 de tinh CV HCN";
+	cout<<"nhap 2 de tinh DT HCN";
+	cout<<"nhap 3 de tinh CV Hinh Vuong";
+	cout<<"nhap 4 de tinh DT Hinh Vuong";
+	
+	switch (tinhCV)
+	{
+	case 1:
+		cout << "Chu vi HCN" << tinhCVhcn() << endl;
+		break;
+	case 2:
+		cout << "Dien tich HCN" << tinhDThcn() << endl;
+		break;
+	case 3:
+		cout << "Chu vi HV" << tinhCVhinhvuong() << endl;
+		break;
+	case 4:
+		cout << "Dien tich HV" << tinhDThinhvuong() << endl;
+		break;	
+	}
+}
+
+int main()
+{
+	int chon;
+	cout<"1.PHEP TOAN";
+	cout<<"2.CHUVVI"
+	cin >> chon;
+	
+	switch (chon)
+	{
+	
+	//tinh chuvi dientich
+	case 1:
+		pheptinh()
+		break;
+	case 2:
+		chuViDienTich()
+		break;
+	
+		
 	default:
 		cout << "Chon sai phep toan";
 		break;
-	}
-	return 0;
+	
+	
 }
